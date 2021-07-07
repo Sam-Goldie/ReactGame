e04ddfed1deb50fcc6de61f804375c1a1f3d4f4c
samgoldie <samuelmgoldie@gmail.com>
2021-07-06 23:03:10 -0400

    Added createBoard function

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Space from './Space.jsx';
 import styles from './Board.css';
 
-const spaces = [1,2,3,4,5,6,7,8,9];
+const createBoard = (n) => {
+  console.log('here I am!');
+  const board = [];
+  let index = 1;
+  for (let i = 0; i < n; i++) {
+    const newArray = [];
+    for (let j = 0; j < n; j++) {
+      newArray.push(index);
+      index++;
+    }
+    board.push(newArray);
+  }
+  return board;
+};
+
+console.log('about to create the board');
+const spaces = createBoard(2);
+console.log(spaces);
+
+// const spaces = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
 const Board = (props) => {
-  <div className={styles.body}></div>
-  return spaces.map((space) => {
+  const [playerPos, updatePlayerPos] = useState([1,1]);
+  return spaces.flat().map((space) => {
     return (
       <Space key={space} spaceid={space}/>
     );
